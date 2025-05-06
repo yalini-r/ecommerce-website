4b862eee47454ce77012c522d3a0daf07adfadf9
yalini-r <yalini.fd14@gmail.com>
2025-05-06 21:45:12 +0530

    Fixed undefined variable 'showAll' in relatedpd.js

--- frontend/src/components/related-pd/relatedpd.js
+++ frontend/src/components/related-pd/relatedpd.js
@@ -1,42 +1,46 @@
-import React, { useContext, useState } from 'react';
-import './relatedpd.css';
-import Item from '../items/items';
-import { ShopContext } from '../../context/shopContext';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import "./relatedpd.css";
+import Item from "../items/items";
+import { ShopContext } from "../../context/shopContext";
+import { useNavigate } from "react-router-dom";
 
 const RelatedProducts = ({ category }) => {
   const { all_product } = useContext(ShopContext);
-  // const [showAll, setShowAll] = useState(false);
+
   const navigate = useNavigate();
 
   // Filter products based on the category
-  const relatedProducts = all_product.filter(item => item.category === category);
+  const relatedProducts = all_product.filter(
+    (item) => item.category === category
+  );
 
   // Show only first 4 products initially
-  const visibleProducts = showAll ? relatedProducts : relatedProducts.slice(0, 4);
+  const visibleProducts = showAll
+    ? relatedProducts
+    : relatedProducts.slice(0, 4);
 
   // Navigate to the correct ShopCategory page
   const handleExploreMore = () => {
-    if (category === 'men') {
-      navigate('/mens');
-    } else if (category === 'women') {
-      navigate('/womens');
-    } else if (category === 'kid') {
-      navigate('/kids');
+    if (category === "men") {
+      navigate("/mens");
+    } else if (category === "women") {
+      navigate("/womens");
+    } else if (category === "kid") {
+      navigate("/kids");
     } else {
-      navigate('/');
+      navigate("/");
     }
   };
 
   return (
-    <div className='rpd'>
+    <div className="rpd">
       <h1>Related Products</h1>
       <hr />
-      <div className='rpd-items'>
+      <div className="rpd-items">
         {visibleProducts.map((item, i) => (
           <Item
-            key={i}  
-            id={item.id}  
+            key={i}
+            id={item.id}
             name={item.name}
             image={item.image}
             new_price={item.new_price}
@@ -46,7 +50,7 @@ const RelatedProducts = ({ category }) => {
       </div>
 
       {/* Explore More Button */}
-      <div className='shp-ctg-loadmore' onClick={handleExploreMore}>
+      <div className="shp-ctg-loadmore" onClick={handleExploreMore}>
         Explore More
       </div>
     </div>
